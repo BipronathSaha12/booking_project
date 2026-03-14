7dc3b320e1c78a76b92768bf44942592980b3585
Bipronath <bipronathsaha@gmail.com>
2026-03-15 00:22:45 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
-set -o errexit 
-pip install -r requirements.txt 
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -x
+
+# Upgrade pip/setuptools/wheel
+python -m pip install --upgrade pip setuptools wheel
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Apply migrations
+python manage.py migrate --no-input
+
+# Collect static files
 python manage.py collectstatic --no-input
-python manage.py migrate 
+
+# Optional: Run tests if you have any
+# python manage.py test
